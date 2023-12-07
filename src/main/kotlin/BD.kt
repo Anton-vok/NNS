c57bd2791ba4e192257bec7df0ede738e0cd2405
A_V <82163471+Anton-vok@users.noreply.github.com>
2023-12-07 15:37:58 +0400

    Реализована бета версия функции add

--- src/main/kotlin/BD.kt
+++ src/main/kotlin/BD.kt
@@ -11,8 +11,12 @@ class Lesson(nName: String, nDay: Int, nClassRoom: MutableList<Int>, nTeacher: S
 
     var color= Color.WHITE
 }
-class NNSBD(){
+class NNSBD(day: Int, classroom: Int){
+    var Lessons= Array<Array<List<Lesson>>>(day) { Array<List<Lesson>>(classroom) { listOf<Lesson>() } }
     fun add(lesson: Lesson){
-
+        var day=lesson.day-1
+        for (i in lesson.classRoom){
+            Lessons[day][i-1]=Lessons[day][i-1]+lesson
+        }
     }
 }
