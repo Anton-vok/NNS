a35d6aeb82010e5875ccd0236f61f7d7cc5dbdfe
A_V <82163471+Anton-vok@users.noreply.github.com>
2023-12-07 18:08:41 +0400

    no comment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-var BD=NNSBD()
+var NNBD=NNSBD(5, 5)
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         mainBox()
